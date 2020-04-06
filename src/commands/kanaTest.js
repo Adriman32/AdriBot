8e823d653d5d7e81f3a89400d066b72a6318907d
NicklasPank <54948334+NicklasPank@users.noreply.github.com>
2020-04-06 04:08:26 -0400

    making sure git works

---
+++ src/commands/kanaTest.js
@@ -0,0 +1,22 @@
+// {COMMAND DESCRIPTION}
+// 
+// Original Version: {DATE}
+// Original Author: {AUTHOR}
+// Current Version: {DATE}
+// Current Author: {AUTHOR}
+
+
+
+//	You can use the variables 'message' and 'args' 
+//	to create your command
+exports.run = (message, args) => {
+
+	var msg = message.content.toUpperCase();
+	var sender = message.member.user.tag;
+	var senderId = message.member.toString();
+	var botId = message.client.user.id.toString();
+	
+	if (msg.includes("HI BOT") {
+        message.channel.send("Hey " + senderId);
+    }
+}
