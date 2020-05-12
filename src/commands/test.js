0b4134cc8ab41be05961bfe9ccb9a38ecf5f36f6
Jeffrey Smith <47571009+jsmith1031@users.noreply.github.com>
2020-05-12 01:50:45 -0400

    testing stuff

--- src/commands/test.js
+++ src/commands/test.js
@@ -1,6 +1,5 @@
 exports.run = (message,args) => {
 	//console.log('You did it!');
-	//console.log('Args: ' + args);
-	var gezz = process.env.pwd
-	message.channel.send(gezz);
+	//console.log('Args: ' + args)
+	message.channel.send(message.channel.id);
 }
