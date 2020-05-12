c01618810131638121b3995b749b6a11e73c6030
Jeffrey Smith <47571009+jsmith1031@users.noreply.github.com>
2020-05-12 01:25:08 -0400

    Update test.js

--- src/commands/test.js
+++ src/commands/test.js
@@ -1,6 +1,6 @@
 exports.run = (message,args) => {
 	//console.log('You did it!');
 	//console.log('Args: ' + args);
-	let gezz = process.env.pwd
+	var gezz = process.env.pwd
 	message.channel.send(gezz);
 }
