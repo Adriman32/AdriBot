834615fd5232118e535636c279dc261da16c8c93
Riverkw <river@kingrw.com>
2020-05-11 23:25:19 -0400

    roast updates

--- src/commands/roast.js
+++ src/commands/roast.js
@@ -2,15 +2,37 @@
 exports.run = (message,args) => {
 	//the argument being passed of the name for who to roast.
 	var roasteeId = args;
+
 	// array of random roasts.
-	var roasts = [  "Smells like wet dog.", 
+	var roasts = [  "smells like wet dog.", 
                     "built like bent iphone.", 
-                    "uses his cats literbox for cereal."];
+                    "uses his cats literbox for cereal.",
+				  	"looks like dead batteries.",
+				  	"look like dog dick.",
+				  	"look like burnt feta cheese.",
+				  	"look like anne frank.",
+				  	"even helen keller think you ugly.",
+				  	"built like a public school basketball hoop.",
+				  	"look like they use sunblock as salad dressing"
+				 ];
+	var joshRoasts = [
+		"forehead bigger than the washington monument.",
+		"born with a receding hairline.",
+		"grew out his hairline to coverup that fivehead."
+	];
+	var marshRoasts = [
+		
+	]
 	/* randomizer to pull from roasts.
  	expected output: a number between 0 and 1 multiplied by the array length.
 	*/
 	var randRoast = Math.floor(Math.random() * roasts.length);
+	var joshRandRoast = Math.floor(Math.random() * joshRoasts.length);
 	
 	// return message including a random roast and the name passed to adriBot.
-	message.channel.send(roasteeId + " " + roasts[randRoast]);
+	if (roasteeId == "josh" || roasteeId == "Josh" || roasteeId == "JOSH") {
+	message.channel.send(roasteeId + " " + joshRoasts[joshRandRoast]);
+	} else {
+		message.channel.send(roasteeId + " " + roasts[randRoast]);
+	}
 }
