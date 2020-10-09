b52be553c21790758c9583c044192f26a50cba08
Adrian Ruvalcaba <adrianr@vt.edu>
2020-10-08 20:13:48 -0400

    Updated Minecraft IP

--- src/commands/mc.js
+++ src/commands/mc.js
@@ -1,4 +1,4 @@
 // Minecraft command
 exports.run = (message,args) => {
-	message.channel.send("Server IP: 198.24.171.42:55674\nPixelmon IP: lodgecraft.mcserver.pw");
+	message.channel.send("Server IP: 131.153.145.138:20080\nPixelmon IP: lodgecraft.mcserver.pw");
 }
